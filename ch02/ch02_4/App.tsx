99fd9e8accaabd51fc261ef678e57ca4fd9f7757
seyeonPark <tpdusdl1001@gmail.com>
2023-08-04 20:34:47 +0200

    update 02-4 컴포넌트의 이벤트 속성 이해하기

---
+++ ch02/ch02_4/App.tsx
@@ -0,0 +1,26 @@
+import react from 'react';
+import {SafeAreaView, Alert, Button, TextInput} from 'react-native';
+import {TouchableOpacity, TouchableHighlight, Text} from 'react-native';
+
+const onPress = () => Alert.alert('home pressed.', 'message');
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Button title="home" onPress={onPress} />
+      <TouchableOpacity onPress={onPress}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight onPress={onPress}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
+      <TextInput
+        placeholder="enter your name"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+      />
+    </SafeAreaView>
+  );
+}
